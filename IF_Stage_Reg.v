5fb72fb478e607967bf802fa5da462f9b0c97721
Hossein Soltanloo <hossein.soltanloo@yahoo.com>
2019-10-16 13:37:33 +0330

    Implement IF Stage Reg

--- IF_Stage_Reg.v
+++ IF_Stage_Reg.v
@@ -4,6 +4,15 @@ module IF_Stage_Reg(
   output reg[31:0] PC, Instruction
 );
 
-  
+  always@(posedge clk, posedge rst) begin
+    if(rst) begin
+      PC <= 0;
+      Instruction <= 0;
+    end
+    else begin
+      PC <= PC_in;
+      Instruction <= Instruction_in;
+    end
+  end
 
 endmodule
