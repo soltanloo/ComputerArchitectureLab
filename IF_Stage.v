5a3364fa1724dac61132c2a787cb1a325da59e8b
Hossein Soltanloo <hossein.soltanloo@yahoo.com>
2019-10-16 13:21:34 +0330

    Add IF Stage boilerplates

---
+++ IF_Stage.v
@@ -0,0 +1,9 @@
+module IF_Stage(
+  input clk, rst, freeze, Branch_taken,
+  input[31:0] BranchAddr,
+  output[31:0] PC, Instruction
+);
+
+  
+
+endmodule
---
+++ IF_Stage_Reg.v
@@ -0,0 +1,9 @@
+module IF_Stage_Reg(
+  input clk, rst, freeze, flush,
+  input[31:0] PC_in, Instruction_in,
+  output reg[31:0] PC, Instruction
+);
+
+  
+
+endmodule
