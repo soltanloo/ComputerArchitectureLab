bce612d0f7f758226a10f7fe118f2ccbb264f4f3
jrazi <j.razi@outlook.com>
2019-10-16 10:34:59 +0330

    Add pc reg

--- IF_Stage.v
+++ IF_Stage.v
@@ -5,6 +5,7 @@ module IF_Stage(
 );
 
   wire[31:0] pcMem[0:5];
+  wire[31:0] pc_in, pc_out;
 
   assign pcMem[0] = 32'b00000000001000100000000000000000;
   assign pcMem[1] = 32'b000000_00011_00100_00000_00000000000;
@@ -13,8 +14,9 @@ module IF_Stage(
   assign pcMem[4] = 32'b000000_01001_01010_00011_00000000000;
   assign pcMem[5] = 32'b000000_01101_01110_00000_00000000000;
 
-  
-  assign PC = Branch_taken ? BranchAddr : PC + 4;
+  reg32 pc(.clk(clk), .rst(rst), .en(~freeze), .reg_in(pc_in), .reg_out(pc_out))
+  assign PC = pc_out + 4;
+  assign pc_in = Branch_taken ? BranchAddr : PC;
   assign Instruction = freeze ? pcMem[PC] : 32'bz;
   
 
---
+++ reg32.v
@@ -0,0 +1,10 @@
+module reg32(input clk, rst, en, input[31:0] reg_in, output reg reg_out)
+
+    always @(posedge clk, posedge rst) begin
+        if (rst) reg_out <= 32'd0;
+        else begin
+            if (en) reg_out <= reg_in;
+        end
+    end
+
+endmodule
