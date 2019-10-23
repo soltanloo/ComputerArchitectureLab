2042ae511b5e0c875dc5a00280ab1e4f7778e192
Hossein Soltanloo <hossein.soltanloo@yahoo.com>
2019-10-23 13:50:00 +0330

    Create register file

--- IF_Stage.v
+++ IF_Stage.v
@@ -3,21 +3,24 @@ module IF_Stage(
   input[31:0] BranchAddr,
   output[31:0] PC, Instruction
 );
-
-  wire[31:0] pcMem[0:5];
+  
   wire[31:0] pc_in, pc_out;
 
-  assign pcMem[0] = 32'b00000000001000100000000000000000;
-  assign pcMem[1] = 32'b000000_00011_00100_00000_00000000000;
-  assign pcMem[2] = 32'b000000_00101_00110_00000_00000000000;
-  assign pcMem[3] = 32'b000000_00111_01000_00010_00000000000;
-  assign pcMem[4] = 32'b000000_01001_01010_00011_00000000000;
-  assign pcMem[5] = 32'b000000_01101_01110_00000_00000000000;
+  always @( * ) begin
+    case (pc_out)
+      0: Instruction <= 32'b000000_00001_00010_00000_00000000000;
+      1: Instruction <= 32'b000000_00011_00100_00000_00000000000;
+      2: Instruction <= 32'b000000_00101_00110_00000_00000000000;
+      3: Instruction <= 32'b000000_00111_01000_00010_00000000000;
+      4: Instruction <= 32'b000000_01001_01010_00011_00000000000;
+      5: Instruction <= 32'b000000_01101_01110_00000_00000000000;
+      default: 
+    endcase
+  end
 
   reg32 pc(.clk(clk), .rst(rst), .en(~freeze), .reg_in(pc_in), .reg_out(pc_out));
   assign PC = pc_out + 1;
   assign pc_in = Branch_taken ? BranchAddr : pc_out >= 32'd5 ? 0 : PC ;
-  assign Instruction = pcMem[pc_out];
-  
+
 
 endmodule
---
+++ RegisterFile.v
@@ -0,0 +1,24 @@
+module RegisterFile(
+  input clk, rst,
+  input[3:0] src1, src2, Dest_wb,
+  input[31:0] Result_WB,
+  input writeBackEn,
+  output[31:0] reg1, reg2
+);
+
+  reg[31:0] registerFile[0:15];
+
+  always @(negedge clk, posedge rst) begin
+    if (rst) begin
+      for (i = 0; i < 15; i++) begin
+        registerFile[i] <= i;
+      end
+    end
+    else if (writeBackEn) begin
+      registerFile[Dest_wb] <= Result_WB;
+    end
+  end
+
+  assign reg1 = registerFile[src1];
+  assign reg2 = registerFile[src2];
+endmodule
