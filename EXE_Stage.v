ab667e63ec809239f27dfcd24cb2d6e8e23428f2
jrazi <j.razi@outlook.com>
2019-10-30 11:10:08 +0330

    Add Val2Generate

--- EXE_Stage.v
+++ EXE_Stage.v
@@ -20,8 +20,8 @@ module EXE_Stage(
   ALU alu(.in1(Val_Rn), .in2(val2GenOut), .EXE_CMD(EXE_CMD), .c(SR[1]), .N(status[3]), .Z(status[2]),
           .C(status[1]), .V(status[0]), .out(ALU_result));
 
-  Val2Generate v2g(.memop(MEM_R_EN | MEM_W_EN), val_rm(Val_Rm),
-                    .imm()Signed_imm_24, .shift_operand(Shift_operand), .out(val2GenOut));
+  Val2Generate v2g(.memrw(MEM_R_EN | MEM_W_EN), .Val_Rm(Val_Rm),
+                    .Imm(Signed_imm_24), .Shift_operand(Shift_operand), .out(val2GenOut));
 
 
 endmodule
---
+++ Val2Generate.v
@@ -0,0 +1,10 @@
+module Val2Generate(
+    input memrw,
+    input[31:0] Val_Rm,
+    input[23:0] Imm,
+    input[11:0] Shift_operand,
+    output[31:0] out
+);
+
+
+endmodule
