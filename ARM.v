634a68541d2a47e1c53c6a3a4d0f1cf808a7ff37
Hossein Soltanloo <hossein.soltanloo@yahoo.com>
2019-10-30 14:40:21 +0330

    Add EXE and ID Stage to top module

--- ARM.v
+++ ARM.v
@@ -304,31 +304,93 @@ input          TD_CLK27;            //	TV Decoder 27MHz CLK
 inout	[35:0]	GPIO_0;					//	GPIO Connection 0
 inout	[35:0]	GPIO_1;					//	GPIO Connection 1
 
-	wire[31:0] PC, Instruction, BranchAddr;
+
+	// Global wires
+	wire[31:0] BranchAddr;
 	wire freeze, Branch_taken, flush;
 	assign freeze = 1'b0;
+	assign hazard = 1'b0;
 	assign Branch_taken = 1'b0;
 	assign flush = 1'b0;
 	assign BranchAddr = 32'b0;
 	assign LEDR = PC[17:0];
 
+	// IF Stage to IF Stage Reg wires
+	wire[31:0] PC, Instruction;
 
-	// ID Stage wires
+	// IF Stage Reg to ID Stage wires
+	wire[31:0] IF_Reg_PC_out, IF_Reg_Ins_out;
+
+	// ID Stage to ID Stage Reg wires
 	wire imm, WB_EN, MEM_R_EN, MEM_W_EN, B, S, Two_src;
-	wire[3:0] EXEC_CMD, Dest, src1, src2;
-	wire[31:0] Val1_Rn, Val_Rm;
+	wire[3:0] EXE_CMD, Dest, src1, src2;
+	wire[31:0] Val_Rn, Val_Rm;
+	wire[11:0] Shift_operand;
+  wire[23:0] Signed_imm_24;
+  wire[3:0] Dest;
+  wire[3:0] src1, src2;
+  wire Two_src;
+  wire[3:0] destAddress;
+  wire[31:0] ID_Stage_PC;
 
-	IF_Stage if_stage(.clk(CLOCK_50), .rst(SW[7]), .freeze(freeze), .Branch_taken(Branch_taken), .BranchAddr(BranchAddr), .PC(PC), .Instruction(Instruction),
-		.out_clk(out_clk));
+	// ID Stage Reg to EXE Stage wires
+	wire ID_out_WB_EN, ID_out_MEM_R_EN, ID_out_MEM_W_EN, ID_out_B, ID_out_S;
+  wire[3:0] ID_out_EXE_CMD;
+  wire[31:0] ID_out_Val_Rm, ID_out_Val_Rn;
+  wire ID_out_imm;
+  wire[11:0] ID_out_Shift_operand;
+  wire[23:0] ID_out_Signed_imm_24;
+  wire[3:0] ID_out_Dest;
+  wire[31:0] ID_out_PC;
 
-	IF_Stage_Reg if_stage_reg(.clk(CLOCK_50), .rst(SW[7]), .freeze(freeze), .flush(flush), .PC_in(PC), .Instruction_in(Instruction));
+	// EXE Stage to EXE Stage Reg wires
+	wire[31:0] ALU_result, Br_addr;
+  
+	// EXE Stage to SR wires
+	wire[3:0] status;
+
+	// EXE Stage Reg out wires
+	wire EXE_Reg_out_WB_EN, EXE_Reg_out_MEM_R_EN, EXE_Reg_out_MEM_W_EN;
+	wire[31:0] EXE_Reg_out_ALU_result, EXE_Reg_out_ST_val, EXE_Reg_out_Dest;
+
+	// SR out wires
+	wire[3:0] SR;
+
+	// WB out wires
+	wire[31:0] Result_WB,
+  wire writeBackEn,
+  wire[3:0] Dest_WB,
+
+	IF_Stage if_stage(
+		.clk(CLOCK_50), .rst(SW[7]), .freeze(freeze), .Branch_taken(Branch_taken), .BranchAddr(BranchAddr),
+		.PC(PC), .Instruction(Instruction), .out_clk(out_clk)
+	);
+
+	IF_Stage_Reg if_stage_reg(
+		.clk(CLOCK_50), .rst(SW[7]), .freeze(freeze), .flush(flush), .PC_in(PC), .Instruction_in(Instruction),
+		.PC(IF_Reg_PC_out), .Instruction(IF_Reg_Ins_out)
+	);
 	
 	ID_Stage id_stage(
-		.clk(CLOCK_50), .rst(SW[7]), .Instruction(Instruction), .Result_WB(), .writeBackEn(), .Dest_WB(), .hazard(), .SR(), .PC_in()
-		.WB_EN(), .MEM_R_EN(), .MEM_W_EN(), .B(), .S(), .EXE_CMD(), .Val_Rn(), .Val_Rm(), .imm(), .Shift_operand(), .Signed_imm_24(), .Dest(), .src1(), .src2(), .Two_src(), .destAddress(), .PC()
+		.clk(CLOCK_50), .rst(SW[7]), .Instruction(IF_Reg_Ins_out), .Result_WB(Result_WB), .writeBackEn(writeBackEn), .Dest_WB(Dest_WB), .hazard(hazard), .SR(SR), .PC_in(IF_Reg_PC_out)
+		.WB_EN(WB_EN), .MEM_R_EN(MEM_R_EN), .MEM_W_EN(MEM_W_EN), .B(B), .S(S), .EXE_CMD(EXE_CMD), .Val_Rn(Val_Rn), .Val_Rm(Val_Rm), .imm(imm), .Shift_operand(Shift_operand), .Signed_imm_24(Signed_imm_24), .Dest(Dest), .src1(src1), .src2(src2), .Two_src(Two_src), .destAddress(destAddress), .PC(ID_Stage_PC)
 	);
 	ID_Stage_Reg id_stage_reg(
-		.clk(CLOCK_50), .rst(SW[7]), .flush(flush), .WB_EN_IN(WB_EN), .MEM_R_EN_IN(), .MEM_W_EN_IN(), .B_IN(), .S_IN(), .EXE_CMD_IN(), .PC_in(), .Val_Rn_IN(), .Val_Rm_IN(), .imm_IN(), .Shift_operand_IN(), .Signed_imm_24_IN(), .Dest_IN()
-		.WB_EN(), .MEM_R_EN(), .MEM_W_EN(), .B(), .S(), .EXE_CMD(), .Val_Rm(), .Val_Rn(), .imm(), .Shift_operand(), .Signed_imm_24(), .Dest(), .PC()
+		.clk(CLOCK_50), .rst(SW[7]), .flush(flush), .WB_EN_IN(WB_EN), .MEM_R_EN_IN(MEM_R_EN), .MEM_W_EN_IN(MEM_W_EN), .B_IN(B), .S_IN(S), .EXE_CMD_IN(EXE_CMD), .PC_in(ID_Stage_PC), .Val_Rn_IN(Val_Rn), .Val_Rm_IN(Val_Rm), .imm_IN(imm), .Shift_operand_IN(Shift_operand), .Signed_imm_24_IN(Signed_imm_24), .Dest_IN(Dest)
+		.WB_EN(ID_out_WB_EN), .MEM_R_EN(ID_out_MEM_R_EN), .MEM_W_EN(ID_out_MEM_W_EN), .B(ID_out_B), .S(ID_out_S), .EXE_CMD(ID_out_EXE_CMD), .Val_Rm(ID_out_Val_Rm), .Val_Rn(ID_out_Val_Rn), .imm(ID_out_imm), .Shift_operand(ID_out_Shift_operand), .Signed_imm_24(ID_out_Signed_imm_24), .Dest(ID_out_Dest), .PC(ID_out_PC)
+	);
+
+	EXE_Stage exe_stage(
+		.clk(CLOCK_50), .EXE_CMD(ID_out_EXE_CMD), .MEM_R_EN(ID_out_MEM_R_EN), .MEM_W_EN(ID_out_MEM_W_EN), .PC(ID_out_PC), .Val_Rn(ID_out_Val_Rn), .Val_Rm(ID_out_Val_Rm), .imm(ID_out_imm), .Shift_operand(ID_out_imm), .Signed_imm_24(ID_out_Signed_imm_24), .SR(SR),
+  	.ALU_result(ALU_result), .Br_addr(Br_addr), .status(status),
+	);
+	EXE_Stage_Reg exe_stage_reg(
+  	.clk(CLOCK_50), .rst(SW[7]), .WB_en_in(ID_out_WB_EN), .MEM_R_EN_in(ID_out_MEM_R_EN), .MEM_W_EN_in(ID_out_MEM_W_EN), .ALU_result_in(ALU_result), .ST_val_in(ID_out_Val_Rm), .Dest_in(ID_out_Dest),
+  	.WB_en(EXE_Reg_out_WB_EN), .MEM_R_EN(EXE_Reg_out_MEM_R_EN), .MEM_W_EN(EXE_Reg_out_MEM_W_EN), .ALU_result(EXE_Reg_out_ALU_result), .ST_val(EXE_Reg_out_ST_val), .Dest(EXE_Reg_out_Dest),
+	);
+
+	reg4neg SR(
+		.clk(CLOCK_50), .rst(SW[7]), .en(S), .reg_in(status),
+		.reg_out(SR)
 	);
 endmodule
