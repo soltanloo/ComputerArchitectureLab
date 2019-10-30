0785e798d94e621cacd40995f16758ecd69baa16
jrazi <j.razi@outlook.com>
2019-10-30 10:46:22 +0330

    Implement 4 bit reg

--- reg32.v
+++ reg32.v
@@ -8,3 +8,14 @@ module reg32(input clk, rst, en, input[31:0] reg_in, output reg[31:0] reg_out);
     end
 
 endmodule
+
+module reg4neg(input clk, rst, en, input[3:0] reg_in, output reg[3:0] reg_out);
+
+    always @(negedge clk, posedge rst) begin
+        if (rst) reg_out <= 4'd0;
+        else begin
+            if (en) reg_out <= reg_in;
+        end
+    end
+
+endmodule
