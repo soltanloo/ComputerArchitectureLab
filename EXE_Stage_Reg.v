581728b5129cc0a5c166e32aae65af298fdfa027
Hossein Soltanloo <hossein.soltanloo@yahoo.com>
2019-10-30 13:54:28 +0330

    Implement EXE Stage Reg

--- EXE_Stage_Reg.v
+++ EXE_Stage_Reg.v
@@ -1,16 +1,18 @@
 module EXE_Stage_Reg(
-  input clk,
-  input rst,
-  input[31:0] PC_in,
-  output reg[31:0] PC
+  input clk, rst, WB_en_in, MEM_R_EN_in, MEM_W_EN_in,
+  input[31:0] ALU_result_in, ST_val_in,
+  input[3:0] Dest_in,
+  output reg WB_en, MEM_R_EN, MEM_W_EN,
+  output reg[31:0] ALU_result, ST_val,
+  output reg[3:0] Dest,
 );
 
   always@(posedge clk, posedge rst) begin
     if(rst) begin
-      PC <= 0;
+      WB_en <= 0; MEM_R_EN <= 0; MEM_W_EN <= 0; ALU_result <= 0; ST_val <= 0; Dest <= 0;
     end
     else begin
-      PC <= PC_in;
+      WB_en <= WB_en_in; MEM_R_EN <= MEM_R_EN_in; MEM_W_EN <= MEM_W_EN_in; ALU_result <= ALU_result_in; ST_val <= ST_val_in; Dest <= Dest_in;
     end
   end
 
