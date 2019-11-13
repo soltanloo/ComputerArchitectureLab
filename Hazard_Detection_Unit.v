b42775388d778e44a47616ee8047bd4bd9c8147f
Hossein Soltanloo <hossein.soltanloo@yahoo.com>
2019-11-13 14:07:43 +0330

    Implement MEM Stage and Hazard Detection Unit

---
+++ Hazard_Detection_Unit.v
@@ -0,0 +1,20 @@
+module Hazard_Detection_Unit(
+  input[3:0] src1,
+  input[3:0] src2,
+  input[3:0] Exe_Dest,
+  input Exe_WB_en,
+  input Two_src,
+  input[3:0] Mem_Dest,
+  input Mem_WB_EN,
+  output hazard_detected
+);
+
+  hazard_detected = (Exe_Dest == 1'b1 && src1 == Exe_Dest) ? 1'b1 : (
+    Mem_WB_EN == 1'b1 && src1 == Mem_Dest ? 1'b1 : (
+      Two_src == 1'b1 && Exe_WB_en == 1'b1 && src2 == Exe_Dest ? 1'b1 : (
+        Two_src == 1'b1 && Mem_WB_EN == 1'b1 && src2 == Mem_Dest ? 1'b1 : 0
+      )
+    )
+  );
+
+endmodule
--- MEM_Stage.v
+++ MEM_Stage.v
@@ -1,10 +1,23 @@
 module MEM_Stage(
-  input clk,
-  input rst,
-  input[31:0] PC_in,
-  output[31:0] PC
-);
+  input clk, MEMread, MEMwrite
+  input[31:0] address, data,
+  output[31:0] MEM_result
+  );
 
-  assign PC = PC_in;
+  reg[31:0] registerFile[0:63];
+  
+  integer i;
+  always @(negedge clk, posedge rst) begin
+    if (rst) begin
+      for (i = 0; i < 15; i= i + 1) begin
+        registerFile[i] <= 32'b0;
+      end
+    end
+    else if (MEMwrite) begin
+      registerFile[address] <= data;
+    end
+  end
+
+  assign MEM_result = MEMread ? registerFile[(address - 1024)[31:2]] : MEM_result;
 
 endmodule
