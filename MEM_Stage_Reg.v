114e0cc04dfa771ae5d05d29c590b2ef7371a0c0
Hossein Soltanloo <hossein.soltanloo@yahoo.com>
2019-12-11 10:54:23 +0330

    Nearly finished

--- MEM_Stage_Reg.v
+++ MEM_Stage_Reg.v
@@ -2,6 +2,7 @@ module MEM_Stage_Reg(
   input clk, rst, WB_en_in, MEM_R_en_in,
   input[31:0] ALU_result_in, Mem_read_value_in,
   input[3:0] Dest_in,
+  input freeze,
   output reg WB_en, MEM_R_en,
   output reg[31:0] ALU_result, Mem_read_value,
   output reg[3:0] Dest
